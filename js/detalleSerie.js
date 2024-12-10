da32173d9ee9d986a1e109c4bf8fb96646737e34
Emmanuel8112 <earaya00259@ufide.ac.cr>
2024-12-09 20:31:43 -0600

    Final

--- js/detalleSerie.js
+++ js/detalleSerie.js
@@ -11,16 +11,57 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     const serie = await response.json();
 
+    
+      let NombreDirector = '';
+      let NombreGenero = '';
+      let NombreProductora = '';
+      let NombreTipoContenido = '';
+  
+    
+  
+      if (serie.ID_Director === 1) {
+        NombreDirector = 'Ridley Scott';
+      } else if (serie.ID_Director === 2) {
+        NombreDirector = 'Randall Einhorn';
+      } else {
+        NombreDirector = 'Director no disponible';
+      }
+  
+      if (serie.ID_Genero === 1) {
+        NombreGenero = 'Acción';
+      } else if (serie.ID_Genero === 2) {
+        NombreGenero = 'Comedia';
+      } else {
+        NombreGenero = 'Género no disponible';
+      }
+  
+      if (serie.ID_Productora === 1) {
+        NombreProductora = 'Scott Free Productions';
+      } else if (serie.ID_Productora === 2) {
+        NombreProductora = 'Deedle-Dee Productions';
+      } else {
+        NombreProductora = 'Productora no disponible';
+      }
+  
+      if (serie.ID_TipoContenido === 1) {
+        NombreTipoContenido = 'Película';
+      } else if (serie.ID_TipoContenido === 2) {
+        NombreTipoContenido = 'Serie';
+      } else {
+        NombreTipoContenido = 'Tipo de contenido no disponible';
+      }
+  
+  
     // Actualizar los elementos HTML con los detalles de la serie
     document.getElementById('titulo').textContent = serie.Título;
     document.getElementById('fecha').textContent = serie.FechaLanzamiento;
     document.getElementById('sinopsis').textContent = serie.Sinopsis;
     document.getElementById('temporadas').textContent = serie.Temporadas;
     document.getElementById('episodios').textContent = serie.EpisodiosTotales;
-    document.getElementById('director').textContent = serie.ID_Director; // Idealmente, obtén el nombre del director desde su ID
-    document.getElementById('genero').textContent = serie.ID_Genero; // Igualmente, obtener el nombre del protagonista
-    document.getElementById('productora').textContent = serie.ID_Productora; // Lo mismo para productora
-    document.getElementById('tipocontenido').textContent = serie.ID_TipoContenido;
+    document.getElementById('director').textContent = NombreDirector; 
+    document.getElementById('genero').textContent = NombreGenero; 
+    document.getElementById('productora').textContent = NombreProductora; 
+    document.getElementById('tipocontenido').textContent = NombreTipoContenido;
     document.getElementById('imagen').src = serie.Imagen;
   } catch (error) {
     console.error('Error al cargar los detalles de la serie:', error);
